cadd046d4f96937ba7edc1cbe4ca0d0a835d9aad
Avik Kundu <1828008@kiit.ac.in>
2019-10-11 21:59:11 +0530

    Added Faculty Section Of Swarnava

--- student_library_faculty.cpp
+++ student_library_faculty.cpp
@@ -10,9 +10,12 @@
 #define FLBSTUD "student.txt"
 #define FLBGRAD "grade.txt"
 using namespace std;
+fstream fil;
 
 
 
+/*================================================================================*/
+/* STUDENT SECTION ------->> AVIK */
 class Student
 {
 	int title;  //Master  = 1, Mr = 2, Miss = 3
@@ -72,7 +75,7 @@ class Student
 
 void Student::getDetails()
 {
-	system("CLS");
+	CLS();
 	cout<<"Enter Title \n(Master = 1, Mr = 2, Miss = 3)  : ";
 	do{
 		title = scan();
@@ -107,7 +110,7 @@ void Student::getDetails()
 
 void insertStudent()			//Insert Student Record in File
 {
-	system("CLS");
+	CLS();
 	Student obj,obj2;
 	char ch;
 	int v=0;
@@ -169,7 +172,7 @@ void insertStudent()			//Insert Student Record in File
 
 int dispStudentRecord()		//Display all Student Records
 {
-	system("CLS");
+	CLS();
 	Student obj;
 	int v=0;
 	fstream fl(FLBSTUD, ios::in|ios::binary);
@@ -196,8 +199,8 @@ int dispStudentRecord()		//Display all Student Records
 
 
 int searchByRollNo(int i)
-{			//Search Record by Roll No., 1 for Class, 2 for Student
-	system("CLS");
+{		
+	CLS();
 	int r;
 	int found=0;
 	Student obj;
@@ -235,7 +238,7 @@ int searchByRollNo(int i)
 
 void delStudentRecord()
 {
-	system("CLS");
+	CLS();
 	Student obj;					//Writes to new file except record to be deleted
 	int f=0;
 	cout<<"\nEnter Roll No. to delete : ";
@@ -244,11 +247,15 @@ void delStudentRecord()
 	cin>>r;
 	fstream fl(FLBSTUD, ios::in|ios::binary);
 	fstream fo("temp.dat", ios::out|ios::binary);
-	while(!fl.eof()){
+	while(!fl.eof())
+	{
 		fl.read((char*)&obj, sizeof(obj));
+		
 		if(fl.eof())
 			break;
-		if (r==obj.retRollNo()){
+			
+		if (r==obj.retRollNo())
+		{
 			cout<<"Record with following info will be deleted :\n\n";
 			obj.printDetails();
 			cout<<"Do you wish to continue ? (Y/N) : ";
@@ -256,9 +263,10 @@ void delStudentRecord()
 				cin>>ch;
 				ch = toupper(ch);
 			}while(ch!='N' && ch!='Y');
-			if(ch=='N'){
+			
+			if(ch=='N')
+			{
 				cout<<"Deletion Aborted !\n";
-
 				fl.close();
 				fo.close();
 				remove("temp.dat");
@@ -281,7 +289,7 @@ void delStudentRecord()
 
 
 /*================================================================================*/
-//SHIVAM'S CODE
+//GRADING SYSTEM ------------->>SHIVAM 
 
 class Grade
 {
@@ -434,7 +442,7 @@ int searchGradeByRollNo(int i)
 }
 
 /*================================================================================*/
-//DIBYA'S CODE
+//PLACEMENT CELL ------->>DIBYAJYOTI
 
 // Students details class.
 class Students
@@ -563,7 +571,7 @@ class placement
     }
     
 /*================================================================================*/
-// LIBRARY MANAGEMENT UNDER UNIVERSITY MANAGEMENT SYSTEM
+// LIBRARY MANAGEMENT UNDER UNIVERSITY MANAGEMENT SYSTEM ------------->>Vaibhav
 
 class Library
 {
@@ -2153,9 +2161,275 @@ void Library::returnBook()
 	(void)_getch();
 	modifyBooklist();
 }    
-    
 
 /*================================================================================*/
+//FACULTY MANAGEMENT ----> SWARNAVA   
+class teacher
+{
+    int a;
+    int phone;
+    string name;
+    string section[10];
+    string subject;
+
+ public:
+
+    void input()
+    {
+        cout<<"\n\t\t\t\t\t Enter teacher name:";
+        cin.ignore();
+        getline(cin,name);
+        cout<<"\n\t\t\t\t\tEnter subject : ";
+        getline(cin,subject);
+        cout<<"\n\t\t\t\t\tenter the phone number: ";
+        cin>>phone;
+        while(1)
+        {
+            cout<<"\n\t\t\t\t\tenter the number of section taught ";
+            cin>>a;
+            if(a<=0)
+                cout<<"\n\t\t\t\t\tsections taught cannot be less than or equal to zero\n";
+            else
+                break;
+        }
+        cout<<"\n\t\t\t\t\tthe section(s) (press enter after giving each section name):";
+        cin.ignore();
+        for(int i=0;i<a;i++)
+            cin>>section[i];
+    }
+    void show()
+    {
+        cout<<"\n\t\t\t\t\tTeacher name: "<<name<<"\n";
+        cout<<"\n\t\t\t\t\tEnter subject : "<<subject<<"\n";
+        cout<<"\n\t\t\t\t\tenter the phone number: "<<phone<<"\n";
+        cout<<"\n\t\t\t\t\tthe sections taught :\n";
+        for(int i=0;i<a;i++)
+            cout<<"\n\t\t\t\t\t"<<section[i]<<"\n";
+    }
+
+    int getph()
+    {
+        return phone;
+    }
+    int geta()
+    {
+        return a;
+    }
+    string getnm()
+    {
+        return name;
+    }
+    string getsub()
+    {
+        return subject;
+    }
+    string getsec(int x)
+    {
+        return section[x];
+    }
+} t;
+
+void Create();
+void Add();
+void Display();
+void Delete();
+void sbn();
+void sbs();
+void sbsec();
+
+
+
+void Create()
+{
+    system("CLS");
+    char ch='y';
+    fil.open("teacher.dat",ios::out| ios::binary);
+    while(ch=='y' || ch =='Y')
+    {
+        t.input();
+        fil.write((char*)&t, sizeof(t));
+        cout<<"\n\t\t\t\t\tWant to Continue (Y/N):";
+        cin>>ch;
+    }
+    fil.close();
+}
+
+void Add()              //Function to Add New Record in Data File
+{
+    system("CLS");
+    char ch='y';
+    fil.open("teacher.dat",ios::app| ios::binary);
+    while(ch=='y' || ch =='Y')
+    {
+        t.input();
+        fil.write((char*)&t, sizeof(t));
+        cout<<"\n\t\t\t\t\tWant to Continue (Y/N):";
+        cin>>ch;
+    }
+    fil.close();
+}
+
+void Display() //Function to Display All Record from Data File
+{
+    system("CLS");
+    fil.open("teacher.dat",ios::in| ios::binary);
+    if(!fil)
+    {
+        cout<<"\n\t\t\t\t\tFile not created\n";
+        exit(0);
+    }
+    fil.read((char*)&t, sizeof(t));
+    if(fil.eof())
+    {
+        cout<<"\n\t\t\t\t\tNo records present.\n";
+        return;
+    }
+    else
+    {
+        cout<<"\n\t\t\t\t\t\tShowing Record(s)\n";
+        while(!fil.eof())
+        {
+            t.show();
+            fil.read((char*)&t, sizeof(t));
+        }
+    }
+    fil.close();
+}
+void sbn()//function which searches record using name of faculty
+{
+    system("CLS");
+    fil.open("teacher.dat",ios::in| ios::binary);
+    if(!fil)
+    {
+        cout<<"\n\t\t\t\t\tFile not created\n";
+        exit(0);
+    }
+    fil.read((char*)&t, sizeof(t));
+    if(fil.eof())
+    {
+        cout<<"\n\t\t\t\t\tNo records present.\n";
+        return;
+    }
+    string s;
+    cout<<"\n\t\t\t\t\tEnter the name of faculty to be searched : ";
+    cin.ignore();
+    getline(cin,s);
+    while(1)
+    {
+        if(s==t.getnm())
+        {
+            cout<<"\n\t\t\t\t\tFaculty found, here are the details :\n";
+            t.show();
+            return;
+        }
+        fil.read((char*)&t, sizeof(t));
+    }
+    cout<<"\n\t\t\t\t\tFaculty does not exist.\n";
+}
+void sbs()//function which searches record using subject taught by faculty
+{
+    system("CLS");
+    fil.open("teacher.dat",ios::in| ios::binary);
+    if(!fil)
+    {
+        cout<<"\n\t\t\t\t\tFile not created\n";
+        exit(0);
+    }
+    fil.read((char*)&t, sizeof(t));
+    if(fil.eof())
+    {
+        cout<<"\n\t\t\t\t\tNo records present.\n";
+        return;
+    }
+    string s;
+    cout<<"\n\t\t\t\t\tEnter the subject to be searched : ";
+    cin.ignore();
+    getline(cin,s);
+    while(1)
+    {
+        if(s==t.getsub())
+        {
+            cout<<"\n\t\t\t\t\tFaculty found, here are the details :\n";
+            t.show();
+            return;
+        }
+        fil.read((char*)&t, sizeof(t));
+    }
+    cout<<"\n\t\t\t\t\tFaculty does not exist.\n";
+}
+void sbsec()//function which searches record using sections taught by faculty
+{
+    system("CLS");
+    fil.open("teacher.dat",ios::in| ios::binary);
+    if(!fil)
+    {
+        cout<<"\n\t\t\t\t\tFile not created\n";
+        exit(0);
+    }
+    fil.read((char*)&t, sizeof(t));
+    if(fil.eof())
+    {
+        cout<<"\n\t\t\t\t\tNo records present.\n";
+        return;
+    }
+    string s;
+    cout<<"\n\t\t\t\t\tEnter the section to be searched : ";
+    cin.ignore();
+    getline(cin,s);
+    int i=0;
+    while(!fil.eof())
+    {
+        for(int j=0;j<t.geta();j++)
+            if(s==t.getsec(j))
+            {
+                i++;
+                if(i==1)
+                    cout<<"\n\t\t\t\t\tFaculty found, here are the details :\n";
+                t.show();
+                cout<<"\n";
+                fil.read((char*)&t, sizeof(t));
+                if(!fil.eof())
+                    j= -1;
+            }
+        fil.read((char*)&t, sizeof(t));
+    }
+    if(i==0)
+        cout<<"\n\t\t\t\t\tFaculty does not exist.\n";
+}
+void Delete() //Function to Delete Particular Record from Data File
+{
+    system("CLS");
+    string srch;
+    fstream o;
+    o.open("copy.dat",ios::out| ios::binary);
+    fil.open("teacher.dat",ios::in| ios::binary);
+    if(!fil)
+    {
+        cout<<"\n\t\t\t\t\tFile not created\n";
+        return ;
+    }
+    cout<<"\n\t\t\t\t\tEnter the name of the teacher whose record is to be deleted :";
+    cin.ignore();
+    getline(cin,srch);
+    fil.read((char*)&t, sizeof(t));
+    while(!fil.eof())
+    {
+        if(srch!=t.getnm())
+        {
+            o.write((char*)&t, sizeof(t));
+            //break;
+        }
+        fil.read((char*)&t, sizeof(t));
+    }
+    fil.close();
+    o.close();
+    remove("teacher.dat");
+    rename("copy.dat","teacher.dat");
+
+}
+
+/*================================================================================*/
+
 int main()
 {
     int a=1,w;
@@ -2244,7 +2518,7 @@ int main()
 					    if(ch1=='2')
 						{
 							fflush(stdin);
-				//			load();
+							load();
 				            do
 							{
 				                CLS();
@@ -2284,13 +2558,14 @@ int main()
 						if(ch1=='3')
 						{
 							fflush(stdin);
+							load();
 							CLS();
 				
-							 RULE("*");
-				                cout <<"\t\t\tSTUDENT INFORMATION SECTION";
-				                RULE("*");
-				                cout<<"\t\t\t    	PLACEMENT CELL";
-				                RULE('-');
+							RULE("*");
+			                cout <<"\t\t\tSTUDENT INFORMATION SECTION";
+			                RULE("*");
+			                cout<<"\t\t\t    	PLACEMENT CELL";
+			                RULE('-');
 				
 							int n,m;
 							cout<<"Enter the number of Students wants to sit in placement: ";
@@ -2351,16 +2626,64 @@ int main()
 			if(ch5=='2')
 			{	
 				fflush(stdin);
+				load();
 				CLS();
 				Library lib;
 				lib.mainMenu();
 			}	
 			
 			if(ch5=='3')
-			{
+			{	
+				int opt;
 				fflush(stdin);
-				CLS();
-				//your code here	
+				load();
+	            do{				
+					CLS();
+					cout<<"\n\t\t\t\t\t* * * * * * FACULTY SECTION * * * * * *\n"<<endl;
+			        cout<<"\n\t\t\t\t\t1. Create Data File"<<endl;
+			        cout<<"\n\t\t\t\t\t2. Add New Record in Data File"<<endl;
+			        cout<<"\n\t\t\t\t\t3. Display Record From Data File"<<endl;
+			        cout<<"\n\t\t\t\t\t4. Delete Particular Record From Data File"<<endl;
+			        cout<<"\n\t\t\t\t\t5. Search by name"<<endl;
+			        cout<<"\n\t\t\t\t\t6. Search by subject taught"<<endl;
+			        cout<<"\n\t\t\t\t\t7. Search by section taught"<<endl;
+			        cout<<"\n\t\t\t\t\t8. Exit From the Program"<<endl;
+			        cout<<"\n\t\t\t\t\t0. Back To Main Menu"<<endl;
+			        cout<<"\n\t\t\t\t\tEnter your choice : ";
+			        cin>>opt;
+			        switch(opt)
+			        {
+			            case 1:
+			                Create();
+			                break;
+			            case 2:
+			                Add();
+			                break;
+			            case 3:
+			                Display();
+			                break;
+			            case 4:
+			                Delete();
+			                break;
+			            case 5:
+			                sbn();
+			                break;
+			            case 6:
+			                sbs();
+			                break;
+			            case 7:
+			                sbsec();
+			                break;
+			            case 8:
+			            	CLS();
+			                exit(0);
+			        }
+			        fflush(stdin);
+		                if(opt)
+		                    cin>>ch;
+		                else
+		                    load();
+    			}while(opt!=0);	
 			}	
 	}while(ch5!='0');
 	CLS();
